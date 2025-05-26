df389dc01872ea24e4e96de5a5c799ed757e4148
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-05-26 22:13:50 +0530

    buld new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { useRef } from "react";
 import TypingBox from "./components/TypingBox"
 import { GlobalStyles } from "./Styles/global.js"
 
 function App() {
+  
   return (
     <div className="canvas">
       <GlobalStyles/>
       <div>hello</div>
       <TypingBox/> 
-      <div>hello</div>
     </div>
   )
 }
